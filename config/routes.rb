36bdc3dfe357732128cbd9cdefaccd53f36867ed
Matthew Koppe <mmmewk@gmail.com>
2018-10-30 23:18:20 -0700

    Remove unfinished routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,15 +12,15 @@ Cognito::Auth::Engine.routes.draw do
   get  '/recover-password', to: "password#edit"
   post '/recover-password', to: "password#update"
 
-  get  '/profile', to: "profile#edit"
-  post '/profile', to: "profile#update"
-  post '/profile/send-attribute-verification', to: "profile#send_attribute_verification"
-  post '/profile/verify_attribute', to: "profile#verify_attribute"
-
-  get  '/admin/users', to: "user#index"
-  get  '/admin/users/new', to: "user#new"
-  post '/admin/users/new', to: "user#create"
-  get  '/admin/users/edit/:username', to: "user#edit", as: "admin_users_edit"
-  post  '/admin/users/edit/:username', to: "user#update"
-  delete '/admin/users/remove/:username', to: "user#remove", as: "admin_users_remove"
+  # get  '/profile', to: "profile#edit"
+  # post '/profile', to: "profile#update"
+  # post '/profile/send-attribute-verification', to: "profile#send_attribute_verification"
+  # post '/profile/verify_attribute', to: "profile#verify_attribute"
+  #
+  # get  '/admin/users', to: "user#index"
+  # get  '/admin/users/new', to: "user#new"
+  # post '/admin/users/new', to: "user#create"
+  # get  '/admin/users/edit/:username', to: "user#edit", as: "admin_users_edit"
+  # post  '/admin/users/edit/:username', to: "user#update"
+  # delete '/admin/users/remove/:username', to: "user#remove", as: "admin_users_remove"
 end
